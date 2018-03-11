cdab2c932af4fbdf37a0df8cf0d0083c22358df6
Christian Legnitto <christian@legnitto.com>
2018-03-11 13:56:28 -0700

    Match reference python version's handling of install id for `upload`

--- src/commands/upload.rs
+++ src/commands/upload.rs
@@ -13,7 +13,16 @@ fn get_connect_url(base_url: Url, uuid: Uuid) -> Result<Url, url::ParseError> {
 
 pub fn go(settings: UploadSettings, api_url: Url) -> Result<Url, Error> {
     // Load install id from a file or generate a new one.
+    // Note: the reference python version doesn't fail when
+    // there is no existing install id, so we don't either.
     let install_info = InstallInfo::new()?;
+
+    // Persist the install id to a file.
+    // Note: the reference python version persists the install id
+    // so we do as well.
+    if !install_info.is_saved {
+        install_info.save()?;
+    }
 
     Ok(get_connect_url(api_url, install_info.id)?)
 }
