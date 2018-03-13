6b5f668a0a4fe0b6398fca21f3fa513756c81a56
Christian Legnitto <christian@legnitto.com>
2018-03-12 22:06:04 -0700

    Make musl and SSL work

--- .ci/setup_musl.sh
+++ .ci/setup_musl.sh
@@ -13,6 +13,6 @@ esac
 rustup target add "$TARGET"
 curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xzf -
 cd openssl-1.0.2l
-CC=musl-gcc ./Configure --prefix="$HOME/openssl-musl" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
+CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
 make -j"$(nproc)"
 make install
