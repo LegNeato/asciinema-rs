9b868ca3d65061a5ea5b7344795abbe8d8caa6cc
Christian Legnitto <christian@legnitto.com>
2018-03-13 13:14:34 -0700

    Normalize url config value
    The `Url` trait treats trailing slashes as significant (https://docs.rs/url/*/url/struct.Url.html#method.join).
    
    This normalizes the url setting when using via the API so users don't have
    to always enter trailing slashes to get proper behavior.
    
    We could have done this when loading the config but we may need to retain
    the original user-entered value for some reason so instead we are normalizing
    at the API interaction layer.

--- src/api.rs
+++ src/api.rs
@@ -10,11 +10,26 @@ pub struct Api {
 
 impl Api {
     pub fn new(base_url: Url) -> Result<Self, Error> {
-        let authentication_url = base_url.join("/connect/")?;
-        let upload_url = base_url.join("/api/asciicasts")?;
+        // The `Url` trait treats trailing slashes as significant:
+        // <https://docs.rs/url/*/url/struct.Url.html#method.join>
+        // We make sure there is always a trailing slash so the joins
+        // below behave correctly.
+        let mut base_raw = base_url.to_string();
+        if let Some(last_char) = base_raw.pop() {
+            if last_char != '/' {
+                base_raw.push(last_char);
+                base_raw.push('/');
+            } else {
+                base_raw.push(last_char);
+            }
+        }
+        let normalized_base = Url::parse(&base_raw)?;
+
+        let authentication_url = normalized_base.join("connect/")?;
+        let upload_url = normalized_base.join("api/asciicasts")?;
         Ok(Api {
             authentication_url,
-            base_url,
+            base_url: normalized_base,
             upload_url,
         })
     }
@@ -48,6 +63,13 @@ mod tests {
     }
 
     #[test]
+    fn normalized_base_url() {
+        let base = Url::parse("http://www.example.com/bar").unwrap();
+        let a = Api::new(base.clone()).unwrap();
+        assert_eq!(a.base_url().to_string(), format!("{}/", base));
+    }
+
+    #[test]
     fn authentication_url() {
         let base = Url::parse("http://www.example.com").unwrap();
         let a = Api::new(base).unwrap();
@@ -58,12 +80,32 @@ mod tests {
     }
 
     #[test]
+    fn normalized_authentication_url() {
+        let base = Url::parse("http://www.example.com/whatever").unwrap();
+        let a = Api::new(base).unwrap();
+        assert_eq!(
+            a.authentication_url().as_str(),
+            "http://www.example.com/whatever/connect/"
+        );
+    }
+
+    #[test]
     fn upload_url() {
         let base = Url::parse("http://www.example.com").unwrap();
         let a = Api::new(base).unwrap();
         assert_eq!(
             a.upload_url().as_str(),
             "http://www.example.com/api/asciicasts"
+        );
+    }
+
+    #[test]
+    fn normalized_upload_url() {
+        let base = Url::parse("http://www.example.com/blah").unwrap();
+        let a = Api::new(base).unwrap();
+        assert_eq!(
+            a.upload_url().as_str(),
+            "http://www.example.com/blah/api/asciicasts"
         );
     }
 }
