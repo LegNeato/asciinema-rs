1c1812ca5cbd63b0f0a385653c223365af858929
Christian Legnitto <christian@legnitto.com>
2018-03-12 21:37:28 -0700

    Use sudo to install musl on CI

--- .ci/setup_musl.sh
+++ .ci/setup_musl.sh
@@ -15,4 +15,4 @@ curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xzf -
 cd openssl-1.0.2l
 CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
 make -j"$(nproc)"
-make install
+sudo make install
